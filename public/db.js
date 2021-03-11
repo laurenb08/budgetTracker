fdf29e57adeb711e3c0ec13af7c1db77b795122c
Lauren Plenger <laurenb08@gmail.com>
2021-03-10 20:17:45 -0800

    added checking database and saving record functionality to db.js

---
+++ public/db.js
@@ -0,0 +1,56 @@
+let db;
+// create a new db request for a "budget" database.
+const request = indexedDB.open("budget", 1);
+
+// create object store called "pending" and set autoIncrement to true
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", {autoIncrement: true});
+};
+
+//if successful, check the database
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+//if error, console log the error
+request.onerror = function(event) {
+    console.log("Oops! " + event.target.errorCode);
+};
+
+//saveRecord
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain. */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    }
+};
+
+window.addEventListener("online", checkDatabase);
