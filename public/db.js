3dd5e369d01031145fc72135966848e35ab195a7
Lauren Plenger <laurenb08@gmail.com>
2021-03-25 19:31:30 -0700

    fixed db issues

--- public/db.js
+++ public/db.js
@@ -1,15 +1,22 @@
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
+
 let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 // create object store called "pending" and set autoIncrement to true
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    db.createObjectStore("pending", {autoIncrement: true});
+    db.createObjectStore("pending", { autoIncrement: true });
 };
 
 //if successful, check the database
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
     if (navigator.onLine) {
         checkDatabase();
@@ -17,7 +24,7 @@ request.onsuccess = function(event) {
 };
 
 //if error, console log the error
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log("Oops! " + event.target.errorCode);
 };
 
@@ -33,7 +40,7 @@ function checkDatabase() {
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
 
-    getAll.onsuccess = function() {
+    getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
             fetch("/api/transaction/bulk", {
                 method: "POST",
@@ -43,12 +50,12 @@ function checkDatabase() {
                     "Content-Type": "application/json"
                 }
             })
-            .then(response => response.json())
-            .then(() => {
-                const transaction = db.transaction(["pending"], "readwrite");
-                const store = transaction.objectStore("pending");
-                store.clear();
-            });
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
         }
     }
 };
